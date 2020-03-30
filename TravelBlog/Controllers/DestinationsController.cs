163e2a729ffe39ee439874298f3489342e0bee64
acolistro <arcolistro@gmail.com>
2020-03-30 10:54:52 -0700

    add destinations controller get an dpost methods
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Mariam Alaeddin <maralaeddin@gmail.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

---
+++ TravelBlog/Controllers/DestinationsController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Models;
+
+namespace TravelBlog.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DestinationsController : ControllerBase
+  {
+    private TravelBlogContext _db;
+    public DestinationsController(TravelBlog db)
+    {
+      _db = db;
+    }
+    [HttpGet]
+    public ActionResult<IEnumerable<Destination>> Get()
+    {
+      return _db.Destinations.ToList();
+    }
+    [HttpPost]
+    public void Post([FromBody] Destination destination)
+    {
+      _db.Destinations.Add(destination);
+      _db.SaveChanges();
+    }
+  }
+}
