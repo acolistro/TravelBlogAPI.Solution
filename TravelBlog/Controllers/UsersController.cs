27d1482db88786c00548e57826faeedc305959bc
acolistro <arcolistro@gmail.com>
2020-03-31 14:29:03 -0700

    add initial files for token authentication
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>

---
+++ TravelBlog/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using TravelBlog.Services;
+using TravelBlog.Models;
+using System.Linq;
+
+namespace TravelBlog.Controllers
+{
+  [Authorize]
+    [TravelBlogController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+      private IUserService _userService;
+
+      public UsersController(IUserService userService)
+      {
+        _userService = userService;
+      }
+
+      // POST /users/authenticate
+      [AllowAnonymous]
+      [HttpPost("authenticate")]
+      public IActionResult Authenticate([FromBody]User userParam)
+      {
+        var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+        if (user == null)
+        {
+          return BadRequest(new { message = "Username or password is incorrect" });
+        }
+        else
+        {
+          return Ok(user);
+        }
+      }
+      
+      // GET /users
+      [HttpGet]
+      public IActionResult GetAll()
+      {
+        var users = _userService.GetAll();
+        return Ok(users);
+      }
+    }
+}
